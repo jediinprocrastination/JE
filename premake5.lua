1937ec86ccffca20530e8d9d1f8c26fa70992c88
Aleksandr Zhedik <a.zhedik@outlook.com>
2020-03-08 20:41:04 +0300

    Add premake exe and first simple premake configuration.

---
+++ premake5.lua
@@ -0,0 +1,91 @@
+
+workspace "Je"
+    architecture "x86"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.architecture}"
+
+    project "Je.Engine"
+        location "Je.Engine"
+        kind "SharedLib"
+        language "C++"
+
+        targetdir ("bin/" .. outputdir)
+        objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "%{prj.name}/src/**.h",
+            "%{prj.name}/src/**.cpp"
+        }
+
+        include
+        {
+            "dependencies/GLEW/include"
+        }
+
+        filter "system:windows"
+            cppdialect "C++17"
+            staticruntime "On"
+            systemversion "latest"
+
+            defines
+            {
+                "JE_PLATFORM_WIN",
+                "JE_BUILD"
+            }
+
+        filter "configurations:Debug"
+            defines "DEBUG"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines "RELEASE"
+            optimize "On"
+
+    project "Je.Playground"
+        location "Je.Playground"
+        kind "ConsoleApp"
+        language "C++"
+
+        targetdir ("bin/" .. outputdir)
+        objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "%{prj.name}/src/**.h",
+            "%{prj.name}/src/**.cpp"
+        }
+
+        include
+        {
+            "Je.Engine/src"
+        }
+
+        links
+        {
+            "Je.Engine"
+        }
+
+        filter "system:windows"
+            cppdialect "C++17"
+            staticruntime "On"
+            systemversion "latest"
+
+            defines
+            {
+                "JE_PLATFORM_WIN"
+            }
+
+        filter "configurations:Debug"
+            defines "DEBUG"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines "RELEASE"
+            optimize "On"
